4f1206ebcacd8080f45df7c4b2e37a3d2d14bd79
Thales <thalesbarbosa.barbosa10@gmail.com>
2025-09-14 17:54:21 -0300

    correção

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Apenas os managers que serão utilizados
         Hospede hospedeManager = new Hospede(null, null, 0, null, false);
         Funcionario funcionarioManager = new Funcionario(null, null, 0, null);
 
@@ -14,24 +13,21 @@ public class Main {
             exibirMenu();
             try {
                 opcao = scanner.nextInt();
-                scanner.nextLine(); // Limpar o buffer
+                scanner.nextLine();
 
                 switch (opcao) {
-                    // --- HÓSPEDES ---
                     case 1: hospedeManager.inserir(scanner); break;
                     case 2: hospedeManager.listar(); break;
                     case 3: hospedeManager.consultar(scanner); break;
                     case 4: hospedeManager.atualizar(scanner); break;
                     case 5: hospedeManager.excluir(scanner); break;
 
-                    // --- FUNCIONÁRIOS ---
                     case 6: funcionarioManager.inserir(scanner); break;
                     case 7: funcionarioManager.listar(); break;
                     case 8: funcionarioManager.consultar(scanner); break;
                     case 9: funcionarioManager.atualizar(scanner); break;
                     case 10: funcionarioManager.excluir(scanner); break;
 
-                    // --- SAIR ---
                     case 0:
                         System.out.println("Saindo do sistema... Obrigado!");
                         break;
@@ -41,7 +37,7 @@ public class Main {
             } catch (InputMismatchException e) {
                 System.err.println("Erro: Por favor, digite um número para a opção.");
                 scanner.nextLine();
-                opcao = -1; // Reseta a opção para continuar no loop
+                opcao = -1;
             }
         } while (opcao != 0);
 
